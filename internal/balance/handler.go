1e4e08db5fe7bad8d8fe02ae17d9ccd130e73016
Armin <armin.azimi@snapp.doctor>
2025-12-19 00:11:42 +0330

    update workspace configuration and improve handler imports

--- internal/balance/handler.go
+++ internal/balance/handler.go
@@ -1,6 +1,10 @@
 package balance
 
-import "github.com/labstack/echo/v4"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func GetBalanceAndHistory(c echo.Context) error {
 	return c.JSON(http.StatusOK, nil)
