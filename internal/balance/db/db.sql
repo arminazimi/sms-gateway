2ba435888b577f0ec5a97a9ecd9d2c0d06588d9f
Armin <armin.azimi@snapp.doctor>
2025-12-19 19:09:06 +0330

    add SMS status tracking table and implement SMS update functionality

--- internal/balance/db/db.sql
+++ internal/balance/db/db.sql
@@ -1,14 +1,8 @@
 CREATE TABLE user_balances (
     user_id BIGINT PRIMARY KEY,
     balance BIGINT NOT NULL DEFAULT 0,
-    last_updated TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    version BIGINT NOT NULL DEFAULT 0
-);
-
--- seed
-insert into user_balances (user_id, balance)
-values (1,100);
-
+    last_updated TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+)ENGINE=InnoDB;
 
 CREATE TABLE user_transactions (
         user_id BIGINT NOT NULL,
@@ -16,4 +10,14 @@ CREATE TABLE user_transactions (
         transaction_type VARCHAR(50) NOT NULL,  -- e.g., 'deposit', 'withdrawal', 'transfer'
         description TEXT,
         created_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+) ENGINE=InnoDB;
+
+CREATE TABLE sms_status (
+        user_id BIGINT NOT NULL,
+        status VARCHAR(50) NOT NULL,
+        type  VARCHAR(50) NOT NULL,
+        recipient VARCHAR(20) NOT NULL,
+        Provider VARCHAR(50)  NULL,
+        created_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+        updated_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
 ) ENGINE=InnoDB;
--- internal/sms/service.go
+++ internal/sms/service.go
@@ -2,9 +2,45 @@ package sms
 
 import (
 	"context"
+	"errors"
+	"sms-gateway/app"
 	"sms-gateway/internal/model"
+	"strings"
+)
+
+type State string
+
+const (
+	Init           State = "init"
+	SendToProvider State = "send-to-provider"
+	Done           State = "done"
 )
 
 func sendSmsToProvider(ctx context.Context, s model.SMS) error {
+	if err := UpdateSMS(ctx, s, Init); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func UpdateSMS(ctx context.Context, s model.SMS, state State) error {
+	if len(s.Recipients) == 0 {
+		return errors.New("no  Recipients")
+	}
+
+	valueStrings := make([]string, 0, len(s.Recipients))
+	valueArgs := make([]any, 0, len(s.Recipients)*4)
+	for _, recipient := range s.Recipients {
+		valueStrings = append(valueStrings, "(?, ?, ?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)")
+		valueArgs = append(valueArgs, s.CustomerID, s.Type, state, recipient)
+	}
+	query := `	INSERT INTO sms_status 
+				(user_id,type, status, recipient, created_at, updated_at) 
+				VALUES ` + strings.Join(valueStrings, ",")
+	if _, err := app.DB.ExecContext(ctx, query, valueArgs...); err != nil {
+		return err
+	}
+
 	return nil
 }
